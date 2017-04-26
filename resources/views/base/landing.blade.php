dfdb278a6d2da3d43b9f8e04f4582e8943082748
Joseph Smith <joe@layerful.com>
2017-04-26 14:37:15 +0100

    Added assets from older site

--- resources/views/base/landing.blade.php
+++ resources/views/base/landing.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.default')
 @section('content')
-
+    This is the landing page
 @stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Look Studios</title>
+        <link rel="shortcut icon" type="image/png" href="/img/favicon.png"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="/css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
