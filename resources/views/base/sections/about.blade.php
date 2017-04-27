947b0daca087f3e16cd48076ee9e91c02ab2009c
Joseph Smith <joe@layerful.com>
2017-04-27 03:51:34 +0100

    Added clickthroughs

--- resources/views/base/sections/about.blade.php
+++ resources/views/base/sections/about.blade.php
@@ -1,11 +1,11 @@
-<div class="section centered" id="about">
+<div class="section centered" id="about" @click.prevent="scrollTo('#us')">
     <div class="container">
         <p class="text-large text-center">Look is design &amp; development.</p>
         <p class="text-large text-center">Operating within the fields of forward thinking furniture, fashion, commerce, and health companies.</p>
     </div>
 </div>
         
-<div class="section centered" id="us">
+<div class="section centered" id="us"  @click.prevent="scrollTo('#labs')">
     <div class="container">
         <ul class="list">
             <li class="title">What we do</li>
--- resources/views/base/sections/hero.blade.php
+++ resources/views/base/sections/hero.blade.php
@@ -1,4 +1,4 @@
-<div class="section top has-background spaced" style="background-image: url('/img/backgrounds/soon.gif')">
+<div class="section top has-background spaced" style="background-image: url('/img/backgrounds/soon.gif')" @click.prevent="scrollTo('#about')">
     @include('global.header')
     <div class="logo-center"></div>
     <a href="#" class="scroll text-center" @click.prevent="scrollTo('#about')"></a>
