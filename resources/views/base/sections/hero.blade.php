15ac8d6cae67256afb763138d1ba4edfe2e3c987
Joseph Smith <joe@layerful.com>
2017-04-28 18:41:34 +0100

    Fixed logo centering

--- resources/views/base/sections/hero.blade.php
+++ resources/views/base/sections/hero.blade.php
@@ -1,4 +1,5 @@
 <div class="section top has-background spaced" style="background-image: url('/img/backgrounds/soon.gif')" @click.stop.prevent="scrollTo('#about')" id="{{isset($id) ? $id : 'hero'}}">
     @include('global.header')
     <div class="logo-center"></div>
+    <div></div>
 </div>
