84c1e1b0933902402216203dd19c6cfb4462e20e
Joseph Smith <joe@layerful.com>
2017-05-03 13:40:53 +0100

    Updated hero

--- resources/views/base/sections/hero.blade.php
+++ resources/views/base/sections/hero.blade.php
@@ -1,4 +1,4 @@
-<div class="section top has-background spaced" style="background-image: url('/img/backgrounds/soon.gif')" @click.prevent="scrollTo('#about')" id="{{isset($id) ? $id : 'hero'}}">
+<div class="item top has-background spaced" style="background-image: url('/img/backgrounds/soon.gif')" @click.prevent="scrollTo('#about')" id="{{isset($id) ? $id : 'hero'}}">
     @if(!isset($menu)) @include('global.header') @endif
     <div class="logo-center"></div>
     <div></div>
